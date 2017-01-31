f5cf89e689911692b6e0f1c3a107f9e6f96082fe
Mizkoeu <zoutianh@math.grinnell.edu>
2017-01-30 22:54:32 -0600

    added test cases/results + minor bug fixes

--- 01-30/src/StringMethods.java
+++ 01-30/src/StringMethods.java
@@ -11,7 +11,7 @@ public class StringMethods {
 	
 	public static void main(String[] args) {
 		System.out.print("StringMethods Exercise: we want to check if a string ends with a"
-				+ "particular repetition of a certain string\n\n"
+				+ " particular repetition of a certain string\n\n"
 				+ "First case: \"foobarbar\", \"bar\", 2\n");
 		if (endsWithRep("foobarbar", "bar", 2))
 			System.out.println("true");
@@ -22,5 +22,16 @@ public class StringMethods {
 			System.out.println("true");
 		else System.out.println("false");
 	}
-
 }
+
+/******************************************Code Testing************************************
+
+StringMethods Exercise: we want to check if a string ends with a particular repetition of a 
+certain string
+
+First case: "foobarbar", "bar", 2
+true
+Second case: "foobarbar", "baz", 1
+false
+
+********************************************************************************************/
