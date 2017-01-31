3bdb4aebdb43f120611cedecfc3cd47bd166b152
Mizkoeu <zoutianh@math.grinnell.edu>
2017-01-30 22:00:26 -0600

    1st Exercise completed

---
+++ 01-30/src/StringMethods.java
@@ -0,0 +1,26 @@
+
+public class StringMethods {
+
+	public static boolean endsWithRep(String s1, String s2, int n){
+		String suffix = new String();
+		for (int i = 0; i < n; i++) {
+			suffix += s2;
+		}
+		return s1.endsWith(suffix);
+	}
+	
+	public static void main(String[] args) {
+		System.out.print("StringMethods Exercise: we want to check if a string ends with a"
+				+ "particular repetition of a certain string\n\n"
+				+ "First case: \"foobarbar\", \"bar\", 2\n");
+		if (endsWithRep("foobarbar", "bar", 2))
+			System.out.println("true");
+		else System.out.println("false");
+		
+		System.out.println("Second case: \"foobarbar\", \"baz\", 1");
+		if (endsWithRep("foobarbar", "baz", 1))
+			System.out.println("true");
+		else System.out.println("false");
+	}
+
+}
--- 01-30/test.java
+++
@@ -1,2 +0,0 @@
-* This is a test file only!
-* I don't like vim at all. Too much trouble.
