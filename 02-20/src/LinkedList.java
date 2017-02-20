0101a9b7a6b7182b7c533f34c36421b88ead9f16
Mike Zou <zoutianh@grinnell.edu>
2017-02-20 01:33:15 -0600

    add 02-20 linked list exercise

---
+++ 02-20/src/LinkedList.java
@@ -0,0 +1,8 @@
+
+public class LinkedList {
+	private Node first;
+	
+	public LinkedList() {
+		this.first = null;
+	}
+}
---
+++ 02-20/src/Node.java
@@ -0,0 +1,16 @@
+
+public class Node {
+	private int value;
+	private Node next;
+	
+	public Node(int value, Node next) {
+		this.value = value;
+		this.next = next;
+	}
+	
+	public Node() {
+		this.value = 0;
+		this.next = null;
+	}
+	
+}
